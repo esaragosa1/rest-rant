940dea13439c9dc3ab89ba332ef113ce343ba07e
esaragosa1 <108439543+esaragosa1@users.noreply.github.com>
2022-11-23 23:48:28 -0800

    Delete functionality implemented

--- views/places/show.jsx
+++ views/places/show.jsx
@@ -7,26 +7,24 @@ function show (data) {
           <main>
 
             <h1>{ data.place.name }</h1>
+            <h2>Rating</h2>
+            <h3> Not Rated</h3>
    
 <a href={`/places/${data.id}/edit`} className="btn btn-warning"> 
   Edit
 </a>     
+     
   
 <form method="POST" action={`/places/${data.id}?_method=DELETE`}> 
   <button type="submit" className="btn btn-danger">
     Delete
   </button>
 </form> 
-    
-    
-     
-
           </main>
         </Def>
         
     )
 }
-
 
 module.exports = show
 
